d5fb0fd0124acfe958085dc092905e3ddbed1628
Maikel Ruiz <maikelrobier@users.noreply.github.com>
2023-01-30 15:58:11 -0600

    Support deploying to Cloud Run

---
+++ scripts/build.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+if [ -z "$1" ]
+    then
+        echo "ERROR: Argument 1 (project_id) is required"
+        exit 1
+    else
+        project_id=$1
+fi
+
+if [ -z "$2" ]
+    then
+        echo "ERROR: Argument 2 (service_name) is required"
+        exit 1
+    else
+        service_name=$2
+fi
+
+if [ -z "$3" ]
+    then
+        echo "ERROR: Argument 3 (image_tag) is required"
+        exit 1
+    else
+        image_tag=$3
+fi
+
+image_uri="gcr.io/$project_id/$service_name:$image_tag"
+
+docker build \
+    -t "$image_uri" \
+    --build-arg SERVICE_NAME_FOLDER=./ \
+    . || exit 1
+
+docker push "$image_uri" || exit 1
---
+++ scripts/deploy.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+if [ -z "$1" ]
+    then
+        echo "ERROR: Argument 1 (project_id) is required"
+        exit 1
+    else
+        project_id=$1
+fi
+
+if [ -z "$2" ]
+    then
+        echo "ERROR: Argument 2 (service_name) is required"
+        exit 1
+    else
+        service_name=$2
+fi
+
+if [ -z "$3" ]
+    then
+        echo "ERROR: Argument 3 (image_tag) is required"
+        exit 1
+    else
+        image_tag=$3
+fi
+
+if [ -z "$4" ]
+    then
+        echo "ERROR: Argument 4 (region) is required"
+        exit 1
+    else
+        region=$4
+fi
+
+gcloud run deploy "$service_name" \
+    --image "gcr.io/$project_id/$service_name:$image_tag" \
+    --region "$region" \
+    --platform managed \
+    --flags-file .run.yaml \
+    --set-env-vars NODE_ENV=production \
+    || exit 1
